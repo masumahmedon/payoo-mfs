fe7be70609434aaac8b63af658e9dd054c45f087
masumahmedon <masumahmed.ict@gmail.com>
2024-09-23 00:32:02 +0600

    added a add money section

--- js/login.js
+++ js/login.js
@@ -2,8 +2,17 @@
 
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('Login button Clicked');
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber)
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are loggged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong phone number or pin')
+    }
 })
